6279d34a381b92a134b91c9a3cad27af4b55483d
root <justinzzk2002@163.com>
2023-11-09 12:23:04 +0800

    update

--- cmd_w3.sh
+++ cmd_w3.sh
@@ -7,7 +7,7 @@
 for i in {70..79}
 do
   dir_num=$(printf "%04d" $i)
-  torchrun --standalone --nnodes=1 --nproc-per-node=4 inference.py --from_pretrained cogvlm-chat --version chat --english --fp16 --input_file=/f_ndata/zekai/test_json/$dir_num.jsonl --output_file=/f_ndata/zekai/caption_data/$dir_num.txt
+  TORCH_CUDA_ARCH_LIST="7.0" torchrun --standalone --nnodes=1 --nproc-per-node=4 inference.py --from_pretrained cogvlm-chat --version chat --english --fp16 --input_file=/f_ndata/zekai/test_json/$dir_num.jsonl --output_file=/f_ndata/zekai/caption_data/$dir_num.txt
 done
 
 # torchrun --standalone --nnodes=1 --nproc-per-node=8 inference.py --from_pretrained cogvlm-chat --version chat --english --fp16 --input_file=test_json/0001.jsonl --output_file=outputs/0001.txt
